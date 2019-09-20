8beba9869faccdb0c5cae70e80b8043a0caed47a
Wang Hankun <1113792580@qq.com>
2019-09-21 00:15:54 +0800

    init_all

---
+++ hitics/lab1/hellolinux.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello World! 1183710106王翰坤.\n");
+	return 0;
+}
---
+++ hitics/lab1/hellowin.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World! 1183710106Íõº²À¤\n");
+    return 0;
+}
