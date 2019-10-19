f08c6d47ae6e5a2d72419f2c0e112458a4081496
Wang Hankun <1113792580@qq.com>
2019-10-20 01:12:44 +0800

    10.20 daily

--- hitics/lab2/const.c
+++ hitics/lab2/const.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char ** argv)
+int main(int argc, int *argv)
 {
     for (int i = 0; i < argc; i++)
-        printf("Argument %d is %s\n", i, argv[i]);
+        printf("Argument %d is %d\n", i, argv[i]);
 
     return 0;
 }
--- hitics/lab2/xyz.c
+++ hitics/lab2/xyz.c
@@ -2,30 +2,13 @@
 
 int x_int = -1183710106;
 
-// const int val_global_dymanic = 1;
-// static const int val_global_static = 2;
-
 int main()
 {
-//  const int val_partial_dymanic = 3;
-//  static const int val_partial_static = 4;
-
     double y_double = 340103200001012333;
     static char z_string[22] = "1183710106-WangHankun";
-
-    // x_int = (float)x_int;
-    // printf("%f\n", (float)x_int);
-    float x_float = (float)x_int;
-    printf("%f\n", x_float);
-    int x_int_float = (int)x_float;
-    printf("%d\n", x_int_float);
-
-    x_int = (int)(float)x_int;
     printf("%d\n", x_int);
     
     printf("x = %d\ny = %lf\nz = %s\n", x_int, y_double, z_string);
-//    printf("%d %d %d %d\n", val_global_dymanic, val_global_static, val_partial_dymanic, val_partial_static);    
-
 
     return 0;
 }
